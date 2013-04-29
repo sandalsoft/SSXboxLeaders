ae961df26b50b619d9206aa5d4693f466f38eab1
Eric Nelson <eric@sandalsoft.com>
2013-04-28 19:04:33 -0500

    Initial Commit

---
+++ SSXboxLeaders/SSXboxLeaders.h
@@ -0,0 +1,13 @@
+//
+//  SSXboxLeaders.h
+//  SSXboxLeaders
+//
+//  Created by Eric Nelson on 4/28/13.
+//  Copyright (c) 2013 Sandalsoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSXboxLeaders : NSObject
+
+@end
---
+++ SSXboxLeaders/SSXboxLeaders.m
@@ -0,0 +1,13 @@
+//
+//  SSXboxLeaders.m
+//  SSXboxLeaders
+//
+//  Created by Eric Nelson on 4/28/13.
+//  Copyright (c) 2013 Sandalsoft. All rights reserved.
+//
+
+#import "SSXboxLeaders.h"
+
+@implementation SSXboxLeaders
+
+@end
